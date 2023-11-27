2f3cfe9e944084a9a473f3bbada7e27b49647516
imalriyad <imalriyad@gmail.com>
2023-11-27 11:32:07 +0600

    banner added with search bar

---
+++ src/Header/Hero.jsx
@@ -0,0 +1,48 @@
+"use client";
+import { SearchBar } from "keep-react";
+import {  MagnifyingGlass } from "phosphor-react";
+import { FaMedal } from "react-icons/fa";
+
+const Hero = () => {
+  return (
+    <div>
+      <div
+        className="hero min-h-screen px-5"
+        style={{
+          backgroundImage:
+            "url(https://i.ibb.co/WK8qgQZ/bgbanner.jpg)",
+            backgroundPosition:'center',
+            backgroundSize:'cover',
+            backgroundRepeat:'no-repeat',
+            objectFit:'fill'
+            
+        }}
+      >
+        <div className="hero-content text-neutral-content md:text-center ">
+          <div className="max-w-2xl mx-auto text-white">
+            <h1 className="mb-5 md:text-5xl text-2xl pt-14 md:pt-28 font-bold">Unleash Your Brilliance: Join Our Thrilling Contests!</h1>
+            <p className="md:mb-10 mb-6 md:text-sm text-gray-300 text-xs ">
+            Welcome to ContestBud, where innovation meets competition! Ignite your passion and showcase your skills in our diverse contests. From cutting-edge business challenges to captivating gaming competitions.
+            </p>
+
+           <div className="max-w-lg mx-auto ">
+           <SearchBar
+            size='md'
+      placeholder="Search Contest"
+      addon={<FaMedal size={20} color="#5E718D" />}
+      addonPosition="left"
+      icon={<MagnifyingGlass size={22} color="#5E718D" />}
+      iconPosition="right"
+     
+    >
+      
+    </SearchBar>
+           </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- src/Header/NavbarCompo.jsx
+++ src/Header/NavbarCompo.jsx
@@ -4,28 +4,28 @@ import { NavLink } from "react-router-dom";
 export const NavbarCompo = () => {
   const navmenu = (
     <>
-      <NavLink className={'hover:text-[#05FFE5] '}>Home</NavLink>
-      <NavLink className={'hover:text-[#05FFE5] '}>Projects</NavLink>
-      <NavLink className={'hover:text-[#05FFE5] '}>News</NavLink>
-      <NavLink className={'hover:text-[#05FFE5] '}>Blogs</NavLink>
+      <NavLink className={'hover:text-[#0ECDB9] '}>Home</NavLink>
+      <NavLink className={'hover:text-[#0ECDB9] '}>Projects</NavLink>
+      <NavLink className={'hover:text-[#0ECDB9] '}>News</NavLink>
+      <NavLink className={'hover:text-[#0ECDB9] '}>Blogs</NavLink>
     </>
   );
   return (
     <Navbar fluid={true} className="bg-[#1F2443] ">
-      <Navbar.Container className="flex items-center py-2 justify-between">
+      <Navbar.Container className="flex items-center py-2 justify-between ">
         <Navbar.Container className="flex items-center justify-between">
           <img src="https://i.ibb.co/Ms0YBtB/contestbud.png" className="md:w-[20%] w-[70%]" alt="" />
 
           <Navbar.Container
             tag="ul"
-            className="lg:flex text-white mr-8 hidden items-center justify-between gap-8"
+            className="lg:flex font-medium text-white mr-8 hidden items-center justify-between gap-8"
           >
             {navmenu}
           </Navbar.Container>
           <Navbar.Collapse collapseType="sidebar">
             <Navbar.Container
               tag="ul"
-              className="flex flex-col gap-5"
+              className="flex font-medium flex-col gap-5"
             >
               {navmenu}
             </Navbar.Container>
@@ -33,7 +33,7 @@ export const NavbarCompo = () => {
         </Navbar.Container>
 
         <Navbar.Container className="flex gap-2">
-          <Button size={'xs'} className="bg-[#0ECDB9]  text-[#1F2443] hover:bg-[#0ECDB9] ">
+          <Button size='xs' className="bg-[#0ECDB9] text-[#1F2443] hover:bg-transparent hover:border border-[#0ECDB9]  hover:text-white">
             Login
           </Button>
           <Navbar.Toggle />
